2728adc7fb870d3f947ffe7326b3ec8d90740f4c
Ruslan Akberov <ruslanakberov@gmail.com>
2018-04-15 15:56:27 +0300

    Sample data added

---
+++ ToDoList/Controllers/ToDoTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Руслан Акберов on 15.04.2018.
+//  Copyright © 2018 Ruslan Akberov. All rights reserved.
+//
+
+import Foundation
